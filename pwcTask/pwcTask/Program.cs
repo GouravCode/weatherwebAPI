c0a5a3907c7adbe11ffaa89bc8297a4fbf4879a9
GouravCode <75539743+GouravCode@users.noreply.github.com>
2023-02-06 22:26:02 +0530

    Add files via upload

---
+++ pwcTask/pwcTask/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace pwcTask
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            decimal latitude = 0.0M;
+            decimal longitude = 0.0M;
+            if (args.Length == 1)
+            {
+
+                string text = File.ReadAllText(@"./cities.json");
+                cityClass[] citiesObject = JsonConvert.DeserializeObject<cityClass[]>(text);
+                for (int i = 0; i < citiesObject.Length; i++)
+                {
+                    if (citiesObject[i].city == args[0])
+                    {
+                        latitude = Convert.ToDecimal(citiesObject[i].lat);
+                        longitude = Convert.ToDecimal(citiesObject[i].lng);
+                        break;
+                    }
+                }
+                using (var client = new WebClient())
+                {
+                    client.Headers.Add("Content-Type:application/json");
+                    client.Headers.Add("Accept:application/json");
+                    var result = client.DownloadString("https://api.open-meteo.com/v1/forecast?latitude="+latitude+"&longitude="+longitude+"&current_weather=true");
+                    Rootobject1 crw = JsonConvert.DeserializeObject<Rootobject1>(result);
+                    Current_Weather crw_details = new Current_Weather();
+                    crw_details = crw.current_weather;
+                    Console.WriteLine("Temperature : "+crw_details.temperature + 
+                        "\nTime : " + crw_details.time+
+                        "\nWeathe Code : " + crw_details.weathercode+ 
+                        "\nWind Direction : " + crw_details.winddirection+
+                        "\nWind Speed: " + crw_details.windspeed);
+                }
+                Console.ReadLine();
+            }
+            else
+                Console.WriteLine("Please Enter valid City Name.");
+
+            
+        }
+    }
+}
---
+++ pwcTask/pwcTask/cityClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pwcTask
+{
+
+    public class Rootobject
+    {
+        public cityClass[] Property1 { get; set; }
+    }
+
+    public class cityClass
+    {
+        public string city { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string country { get; set; }
+        public string iso2 { get; set; }
+        public string admin_name { get; set; }
+        public string capital { get; set; }
+        public string population { get; set; }
+        public string population_proper { get; set; }
+    }
+
+    public class Rootobject1
+    {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public float generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezone_abbreviation { get; set; }
+        public float elevation { get; set; }
+        public Current_Weather current_weather { get; set; }
+    }
+
+    public class Current_Weather
+    {
+        public float temperature { get; set; }
+        public float windspeed { get; set; }
+        public float winddirection { get; set; }
+        public int weathercode { get; set; }
+        public string time { get; set; }
+    }
+
+
+}
